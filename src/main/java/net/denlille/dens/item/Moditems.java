9e5679ec286bded496d8b5947c9f65939ec2049e
Denlille <nelekk33@gmail.com>
2022-12-08 13:08:37 +0100

    test

---
+++ src/main/java/net/denlille/dens/item/Moditems.java
@@ -0,0 +1,21 @@
+package net.denlille.dens.item;
+
+import net.denlille.dens.Dens;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class Moditems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Dens.MOD_ID);
+
+    public static final RegistryObject<Item> COBALT = ITEMS.register("cobalt",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
